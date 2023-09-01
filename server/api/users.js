90c92ea6e9e039aac8c2e8260ec92937fd14c16c
winniemei <wmei91@gmail.com>
2023-09-01 17:14:39 -0400

    update works

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers, getUserById } = require('../db/helpers/users')
+const { getAllUsers, getUserById, updateUser } = require('../db/helpers/users')
 
 // GET - /api/users - get all users
 router.get('/', async (req, res, next) => {
@@ -9,7 +9,7 @@ router.get('/', async (req, res, next) => {
         const users = await getAllUsers();
         res.send(users);
     }
-    catch(error) {
+    catch (error) {
         next(error);
     }
 })
@@ -21,9 +21,39 @@ router.get('/:id', async (req, res, next) => {
         const rows = await getUserById(req.params.id);
         res.send(rows);
     } catch (error) {
-        console.log('what the heck')
         next(error);
     }
 });
+
+// PUT - /api/users - update a new user
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const user = await updateUser(req.params.id, req.body);
+        res.send(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST - /api/cats - create a new cat
+router.post('/', async (req, res, next) => {
+    try {
+        const newUser = await updateUser(user);
+        if (newUser) {
+            res.send(newUser);
+        } else {
+            next({
+                name: 'CreateUserError',
+                message: 'There was an error creating the user'
+            });
+        }
+    }
+    catch (error) {
+    next(error);
+}
+});
+
+
 
 module.exports = router;
--- server/db/helpers/users.js
+++ server/db/helpers/users.js
@@ -1,48 +1,80 @@
 const client = require('../client');
 
-const createUser = async({ username, password }) => {
-    try {
-        const {
-            rows: [user]
-        } = await client.query(
-        `
+const createUser = async ({ username, password }) => {
+  try {
+    const {
+      rows: [user]
+    } = await client.query(
+      `
         INSERT INTO users(username, password)
         VALUES ($1, $2)
         RETURNING *;
         `,
-        [username, password]
-        );
-        return user;
-    } catch (error) {
-        throw error
-    }
+      [username, password]
+    );
+    return user;
+  } catch (error) {
+    throw error
+  }
 }
 
 // GET - /api/users - get all users
 async function getAllUsers() {
-    try {
-        const { rows } = await client.query(`
+  try {
+    const { rows } = await client.query(`
         SELECT * FROM users;
         `)
-        return rows;
-    } catch(error) {
-        throw error;
-    }
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
 // GET - /api/users/:id - get user by id
 async function getUserById(id) {
-    try {
-      const { rows: [user] } = await client.query(`
+  try {
+    const { rows: [user] } = await client.query(`
         SELECT * FROM users
         WHERE users_id = $1
-      `, [id]) ;
-      return user;
-    } catch (error) {
-      throw error;
-    }
+      `, [id]);
+    return user;
+  } catch (error) {
+    throw error;
   }
+}
+
+// POST - /api/users - create a new user
+async function updateUser({username, password}) {
+  try {
+    const {rows: [user]} = await client.query(`
+        INSERT INTO users ("username", "password")
+        VALUES($1, $2)
+        RETURNING *;
+    `, [username, password]);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 
 
 
-module.exports = { createUser, getAllUsers, getUserById }
+// PUT - /api/users/:id - update a user
+async function updateUser(id, body) {
+  try {
+    const { rows } = await client.query(
+    `
+    UPDATE users
+    SET username = '${body.username}', password = '${body.password}' WHERE users_id = ${id}
+    RETURNING *;
+    `
+    );
+    console.log("updated user", rows);
+    return rows;
+    }
+     catch (error) {
+      throw error;
+  }
+}
+
+module.exports = { createUser, getAllUsers, getUserById, updateUser }
