55739ef32a43bb8d796b3840dcd6f0b53b036bb6
winniemei <wmei91@gmail.com>
2023-09-01 11:54:28 -0400

    more api stuff

--- server/api/users.js
+++ server/api/users.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { getAllUsers } = require('../db/helpers/users')
 
 // GET - /api/users - get all users
-
 router.get('/users', async (req, res, next) => {
     try {
         const users = await getAllUsers();
@@ -14,5 +13,15 @@ router.get('/users', async (req, res, next) => {
         next(error);
     }
 })
+
+// GET - /api/users/:userId - get user by id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const rows = await getUserById(req.params.id);
+        res.send(rows);
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
--- server/db/helpers/users.js
+++ server/db/helpers/users.js
@@ -21,11 +21,27 @@ const createUser = async({ username, password }) => {
 // GET - /api/users - get all users
 async function getAllUsers() {
     try {
-        const rows = data.users;
+        const { rows } = await client.query(`
+        SELECT * FROM users;`)
         return rows;
     } catch(error) {
         throw error;
     }
 }
+
+// GET - /api/users/:id - get user by id
+async function getUserById({id}) {
+    try {
+      const { rows } = await client.query(`
+        SELECT * FROM users
+        WHERE "users_id" = ${id}
+      `);
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
 
 module.exports = { createUser, getAllUsers }
