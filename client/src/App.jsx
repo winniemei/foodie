37d7cf8726e73cd1381a0cd9a51a7d2ae50b6f31
winniemei <wmei91@gmail.com>
2023-09-05 16:35:48 -0400

    added more components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div id="app-container">
-        <h1>APP COMPONENT</h1>
+        <h1>APP</h1>
         <NavBar />
         <MainSection />
       </div>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-    
+    return (
+    <>
+    <div id="home-container">
+    <h1>HOME</h1>
+    <h1>What cha got in the fridge?</h1>
+    </div>
+    </>
+    )
 }
