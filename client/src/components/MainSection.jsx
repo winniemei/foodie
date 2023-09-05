7d24bbf382485eedf09375fdc3fc9b00f93f88a3
winniemei <wmei91@gmail.com>
2023-09-05 16:23:14 -0400

    added more routes

--- client/src/components/MainSection.jsx
+++ client/src/components/MainSection.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import AllRecipes from "./Recipes";
+import Home from "./Home";
+import LogIn from "./LogIn";
+import LogOut from "./LogOut";
 
 export default function MainSection() {
 	return (
 		<div id="main-section">
             <h1>MAIN SECTION</h1>
 			<Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/logout" element={<LogOut />} />
 				<Route path="/recipes" element={<AllRecipes />} />
 			</Routes>
 		</div>
