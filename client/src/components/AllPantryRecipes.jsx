0f7cadd30bdd92b88833508d205e837db010a566
winniemei <wmei91@gmail.com>
2023-09-06 19:30:46 -0400

    getting recipe id

--- client/src/components/AllPantryRecipes.jsx
+++ client/src/components/AllPantryRecipes.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { fetchAllRecipes, fetchRecipesIngredientsJunctionTable } from "../fetching.js"
 
 export default function AllPantryRecipes({ checkedPantryIngredients, ingredients }) {
-    const [recipes, setRecipes] = useState([]);
+	const [recipes, setRecipes] = useState([]);
 	const [pantryRecipes, setPantryRecipes] = useState([]);
+	const [chosenRecipes, setChosenRecipes] = useState([]);
 	const checkedPantryIngredientsArray = [...checkedPantryIngredients]
 
 	// render all recipes    
@@ -12,54 +13,38 @@ export default function AllPantryRecipes({ checkedPantryIngredients, ingredients
 			const response = await fetchAllRecipes();
 			console.log(response);
 			setRecipes(response);
-            console.log('ALL RECIPES', response)
+			console.log('ALL RECIPES', response)
 		}
 		getAllRecipes();
 	}, []);
 
-    useEffect(() => {
+	useEffect(() => {
 		async function getAllRecipesIngredients() {
 			const junctionResponse = await fetchRecipesIngredientsJunctionTable();
-            setPantryRecipes(junctionResponse);
-            console.log('ALL PANTRY RECIPES', junctionResponse);
+			setPantryRecipes(junctionResponse);
+			console.log('ALL PANTRY RECIPES', junctionResponse);
 
 		}
 		getAllRecipesIngredients();
 	}, []);
 
-	// check if pantry ingredient ids are contained in Pantry recipes
-	// iterate thru the junction table, return recipesid for when checkedPantryIngredientsArray id equals to ingredientsid of pantryRecipes
-	const chosenRecipes = []; 
-
-	pantryRecipes.map((pantryRecipe) => {
-		if (pantryRecipe.ingredientsid in checkedPantryIngredientsArray) {
-			chosenRecipes.push(pantryRecipe.recipesid)
-			console.log('CHOSEN RECIPES', chosenRecipes);
-			return pantryRecipe.recipesid;
+	console.log('LOGGING PANTRY RECIPES', pantryRecipes);
+	
+	{pantryRecipes.map((pantryRecipe) =>  {
+		console.log('pantryRecipe.ingredientsid', pantryRecipe.ingredientsid);
+		console.log('LOGGING CHECKED PANTRY INGREDIENTS', checkedPantryIngredientsArray);
+		if (checkedPantryIngredientsArray.includes(pantryRecipe.ingredientsid)) {
+			console.log('recipeid', pantryRecipe.recipesid)
 		}
-	})
-
+	})}
 	return (
 		<div>
 			<div>
 				<div id="recipe-container">
 					<h1>Here's some food that you can make!</h1>
-					
-					{/* {pantryRecipes.map((recipe) => {
-						return(
-							<>
-							<div id="each-recipe">
-							<h3>{recipe.title}</h3>
-							<h3>Ingredients: {recipe.portions}</h3>
-							<h3>Minutes: {recipe.cookingtime}</h3>
-
-							<iframe width="478" height="850" src={recipe.video}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-							
-							<h3></h3>
-							</div>
-							</>
-						)
-					})} */}
+					<>
+					{}
+					</>
 				</div>
 			</div>
 		</div>
