ca1885e9b020f3e0cd4890b533c0b1b51dbeab28
winniemei <wmei91@gmail.com>
2023-09-06 15:19:52 -0400

    get pantry ingredients to render

--- client/src/components/MyPantry.jsx
+++ client/src/components/MyPantry.jsx
@@ -3,20 +3,10 @@ import AllIngredients from "./Ingredients";
 export default function MyPantry({ checkedPantryIngredients, ingredients }) {
 
     const checkedPantryIngredientsArray = [...checkedPantryIngredients]
-    console.log(checkedPantryIngredientsArray);
 
-    // filter out ingredients that are not pantry ingredients
-    const validIngredients = checkedPantryIngredientsArray.filter((pantryIngredient) => {
-        for (const ingredient of ingredients) {
-            if (ingredient.ingredients_id === pantryIngredient) {
-                return true;
-            }
-        }
-        return false;
-    })
     return (
         <>
-            {validIngredients.map((pantryIngredient) => {
+            {checkedPantryIngredientsArray.map((pantryIngredient) => {
                 return (
                     <div id="each-pantry-ingredient">
                         <p>{ingredients.find((ingredient) => ingredient.ingredients_id === pantryIngredient).name}</p>
