750629befd4c27b08b366d439e9475a274656ec4
winniemei <wmei91@gmail.com>
2023-09-06 19:48:09 -0400

    get filtered recipes working

--- client/src/components/AllPantryRecipes.jsx
+++ client/src/components/AllPantryRecipes.jsx
@@ -29,12 +29,15 @@ export default function AllPantryRecipes({ checkedPantryIngredients, ingredients
 	}, []);
 
 	console.log('LOGGING PANTRY RECIPES', pantryRecipes);
-	
+	const filteredRecipes = [];
 	{pantryRecipes.map((pantryRecipe) =>  {
 		console.log('pantryRecipe.ingredientsid', pantryRecipe.ingredientsid);
 		console.log('LOGGING CHECKED PANTRY INGREDIENTS', checkedPantryIngredientsArray);
-		if (checkedPantryIngredientsArray.includes(pantryRecipe.ingredientsid)) {
-			console.log('recipeid', pantryRecipe.recipesid)
+		if (checkedPantryIngredientsArray.includes(pantryRecipe.ingredientsid)) 
+		{
+			console.log('recipeid', pantryRecipe.recipesid);
+			filteredRecipes.push(pantryRecipe.recipesid);
+			console.log('filtered recipes', filteredRecipes)
 		}
 	})}
 	return (
@@ -42,9 +45,13 @@ export default function AllPantryRecipes({ checkedPantryIngredients, ingredients
 			<div>
 				<div id="recipe-container">
 					<h1>Here's some food that you can make!</h1>
-					<>
-					{}
-					</>
+					
+					{recipes.map((recipe) => 
+					{
+						console.log('recipe', recipe)
+						console.log(filteredRecipes)
+					})}
+					
 				</div>
 			</div>
 		</div>
