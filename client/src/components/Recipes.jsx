0a6da7fc42b38400b180fd85a083640aded72fbe
winniemei <wmei91@gmail.com>
2023-09-07 17:13:26 -0400

    style my search bar

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -36,6 +36,7 @@ export default function AllRecipes() {
 				<label>
 					Search:{" "}
 					<input
+						className="inputField"
 						type="text"
 						placeholder="Search"
 						onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
