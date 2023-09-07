b2051f10e403e17ba343c883163fe8398e5a5ae6
winniemei <wmei91@gmail.com>
2023-09-06 21:21:34 -0400

    get rid of unnecessary code

--- client/src/components/MainSection.jsx
+++ client/src/components/MainSection.jsx
@@ -4,8 +4,6 @@ import Home from "./Home";
 import LogIn from "./LogIn";
 import LogOut from "./LogOut";
 import AllIngredients from "./Ingredients";
-import MyPantry from "./MyPantry";
-import AllPantryRecipes from "./AllPantryRecipes";
 
 export default function MainSection() {
 	return (
@@ -16,8 +14,6 @@ export default function MainSection() {
                 <Route path="/logout" element={<LogOut />} />
 				<Route path="/recipes" element={<AllRecipes />} />
                 <Route path="/ingredients" element={<AllIngredients />} />
-				<Route path="/myPantry" element={<MyPantry />} />
-				<Route path="/allPantryRecipes" element={<AllPantryRecipes />} />
 			</Routes>
 		</div>
 	);
