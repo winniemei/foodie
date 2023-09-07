3ad727e50e61f762c15af36d451042de6fccbe08
winniemei <wmei91@gmail.com>
2023-09-07 12:41:26 -0400

    fix delete

--- client/src/components/DeleteRecipe.jsx
+++ client/src/components/DeleteRecipe.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
-import AllRecipes from "./Recipes";
 import { deleteRecipe } from "../fetching";
 
 
