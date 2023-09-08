fd12eed5a2d4d302c973fe1e723af0201d4e70b5
winniemei <wmei91@gmail.com>
2023-09-08 10:03:56 -0400

    add crab legs

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -3,7 +3,8 @@
 // Recipes
 const recipes = [
     { title: "Fried eggs", portions: ['2 eggs','1/4 stick of butter'], cookingTime: 15, is_vegetarian: true, description: "1. Heat a skillet over medium-low, allowing it to fully pre-heat (3-5 minutes) 2. Add the butter (or fat of choice) and swirl to coat the surface of the skillet. 3. Crack the egg into the frying pan and cook until the whites are set and the yolk is still liquid (sunny side up). 4.For over easy, over medium, or over hard, cook the egg until the whites are 50-75% set, then flip the egg and continue cooking on the second side until the yolk is cooked to your desired doneness. 5. Transfer the egg to a plate and season with salt and pepper. Enjoy!", video: 'https://www.youtube.com/embed/sdk0CEfTaIw', userId: 1 },
-    { title: "Salmon avocado bowl", portions: ['1 filet of salmon', '1 avocado'], cookingTime: 20, is_vegetarian: true, description: "you put some avo and salmon together", video: 'https://www.youtube.com/embed/WM_A_ehCt_4', userId: 2 }
+    { title: "Salmon avocado bowl", portions: ['1 filet of salmon', '1 avocado'], cookingTime: 20, is_vegetarian: true, description: "you put some avo and salmon together", video: 'https://www.youtube.com/embed/WM_A_ehCt_4', userId: 2 },
+    { title: "Crab", portions: ['1 crab'], cookingTime: 20, is_vegetarian: true, description: "1. Steam crab legs for 5 - 7 minutes 2. Put butter, paprika, salt into pan and fry crab legs", video: 'https://www.youtube.com/embed/tz2IVtgRgJg', userId: 2 }
 ]
 
 // Users
@@ -17,14 +18,16 @@ const users = [
 const ingredients = [
     { name: 'eggs', type: 'protein', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Fried_Egg_2.jpg'}, 
     { name: 'salmon', type: 'fish', image: 'https://www.google.com/aclk?sa=l&ai=DChcSEwiqyPX13oeBAxWq8ZQJHVbFD3EYABAHGgJ5bQ&ase=2&gclid=Cj0KCQjw9MCnBhCYARIsAB1WQVVq92yqUo9l-_MBwq_585o6Q79egElUYWL4xjfBbfQnIxj8i6Gz_CMaAniCEALw_wcB&sig=AOD64_3WnKrZIzawVhN7GGTqt3FNveyMig&ctype=5&nis=5&adurl&ved=2ahUKEwj7vuP13oeBAxWeH2IAHZMKCDIQvhd6BQgBEI8B'}, 
-    { name: 'avocado', type: 'veggie', image: "https://www.google.com/aclk?sa=l&ai=DChcSEwirm4yL34eBAxUJjcgKHfqQA0AYABALGgJxdQ&ase=2&gclid=Cj0KCQjw9MCnBhCYARIsAB1WQVVNW6WQUPfKmo09UxWpb_Z1ST6_t4HMaOAeVSI7h8xkhGOztrY_qkcaAkhNEALw_wcB&sig=AOD64_1iNBb2gjhx_iCtR-AkX9HxPaqygw&ctype=5&nis=5&adurl&ved=2ahUKEwiKsvyK34eBAxWXIWIAHeqiD0IQvhd6BQgBEKAB"}
+    { name: 'avocado', type: 'veggie', image: "https://www.google.com/aclk?sa=l&ai=DChcSEwirm4yL34eBAxUJjcgKHfqQA0AYABALGgJxdQ&ase=2&gclid=Cj0KCQjw9MCnBhCYARIsAB1WQVVNW6WQUPfKmo09UxWpb_Z1ST6_t4HMaOAeVSI7h8xkhGOztrY_qkcaAkhNEALw_wcB&sig=AOD64_1iNBb2gjhx_iCtR-AkX9HxPaqygw&ctype=5&nis=5&adurl&ved=2ahUKEwiKsvyK34eBAxWXIWIAHeqiD0IQvhd6BQgBEKAB"},
+    { name: 'crab', type: 'seafood', image: "https://www.google.com/aclk?sa=l&ai=DChcSEwirm4yL34eBAxUJjcgKHfqQA0AYABALGgJxdQ&ase=2&gclid=Cj0KCQjw9MCnBhCYARIsAB1WQVVNW6WQUPfKmo09UxWpb_Z1ST6_t4HMaOAeVSI7h8xkhGOztrY_qkcaAkhNEALw_wcB&sig=AOD64_1iNBb2gjhx_iCtR-AkX9HxPaqygw&ctype=5&nis=5&adurl&ved=2ahUKEwiKsvyK34eBAxWXIWIAHeqiD0IQvhd6BQgBEKAB"}
 ]
 
 
 const recipesIngredients = [
     { recipesId: 1, ingredientsId: 1},
     { recipesId: 2, ingredientsId: 2},
-    { recipesId: 2, ingredientsId: 3}
+    { recipesId: 2, ingredientsId: 3},
+    { recipesId: 3, ingredientsId: 4},
 ]
 
 const usersIngredients = [
