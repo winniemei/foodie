0c35fabb334ff99f929e5d1d144ab186e5c91620
winniemei <wmei91@gmail.com>
2023-09-12 18:08:41 -0400

    added some comments

--- server/api/users.js
+++ server/api/users.js
@@ -68,15 +68,15 @@ router.post('/register', async (req, res, next) => {
         const { username, password } = req.body
         //hashing the password
         const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-        //sending username and hashed pw to database
+        //send username and hashed pw to database
         const user = await createUser({ username, password: hashedPassword })
         //removing password from user object for security reasons
         delete user.password
 
-        //creating our token
+        //create our token
         const token = jwt.sign(user, JWT_SECRET)
 
-        //attaching a cookie to our response using the token that we created
+        //attach cookie to our response using the token that we created
         res.cookie('token', token, {
             sameSite: 'strict',
             httpOnly: true,
@@ -86,8 +86,6 @@ router.post('/register', async (req, res, next) => {
         console.log('token', token)
 
         delete user.password
-        // console.log(res)
-
         res.send({user})
 
     } catch(error) {
@@ -106,9 +104,9 @@ router.post('/login', async (req, res, next) => {
 
         delete user.password
         if (validPassword) {
-            //creating our token
+            //create our token
             const token = jwt.sign(user, JWT_SECRET)
-            //attaching a cookie to our response using the token that we created
+            //attach cookie to our response using the token that we created
             res.cookie('token', token, {
                 sameSite: 'strict',
                 httpOnly: true,
