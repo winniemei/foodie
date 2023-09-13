54742ef557dcc922e17d2c3fce5d211417e6ccac
winniemei <wmei91@gmail.com>
2023-09-13 18:10:10 -0400

    food coma gif

--- client/src/components/LogOut.jsx
+++ client/src/components/LogOut.jsx
@@ -13,9 +13,9 @@ export default function LogOut({ token, setToken }) {
 
     return (
         <>
-            <div id="log-in-container">
+            <div>
                 <h1>You're logged out</h1>
-
+                <img id="food-coma" src="https://i.pinimg.com/originals/ce/f2/29/cef22956f96f6af957fb1b5b6508ec0f.gif"></img>
             </div>
         </>
     )
