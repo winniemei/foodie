c4be47894cc73b01e0803cf530721a8d2c672aa6
winniemei <wmei91@gmail.com>
2023-09-13 11:42:32 -0400

    add cypress testing

---
+++ client/src/components/LogIn.cy.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import LogIn from './LogIn'
+import { BrowserRouter } from 'react-router-dom'
+
+describe('<LogIn />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>)
+  });
+
+  it('should autofocus on the username input', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>)
+
+      cy.focused().should('have.id', 'username')
+  });
+
+  it('checks that there is an h1 with text login', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>)
+
+      cy.get('h1').contains('Log In')
+  });
+
+  it('checks that there is an h1 with text login', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>)
+
+      cy.get('h1').contains('Log In')
+  });
+
+})
--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -32,7 +32,7 @@ export default function LogIn({ setToken }) {
             <div id="log-in-container">
                 <h1>Log In</h1>
                 <form onSubmit={handleSubmit}>
-                    <input className="inputField" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} /><br/ >
+                    <input id="username" className="inputField" autoFocus placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} /><br/ >
                     <input className="inputField" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} /><br />
                     <button type="submit">SIGN IN</button>
                     <button id="button-detail" onClick={handleRegister}>REGISTER</button>
